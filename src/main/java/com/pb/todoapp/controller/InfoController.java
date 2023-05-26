3d361733120c0530cd202fb9d8bf37e0815f3efc
pb <budziakp@gmail.com>
2023-05-26 16:54:26 +0200

    add InfoController and application.properties

---
+++ src/main/java/com/pb/todoapp/controller/InfoController.java
@@ -0,0 +1,27 @@
+package com.pb.todoapp.controller;
+
+import com.pb.todoapp.TaskConfigurationProperties;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class InfoController {
+    private DataSourceProperties dataSource;
+    private TaskConfigurationProperties myProp;
+
+    public InfoController(final DataSourceProperties dataSource, final TaskConfigurationProperties myProp) {
+        this.dataSource = dataSource;
+        this.myProp = myProp;
+    }
+
+    @GetMapping("/info/url")
+    String url(){
+        return dataSource.getUrl();
+    }
+
+    @GetMapping("/info/prop")
+    boolean myProp() {
+        return myProp.getTemplate().isAllowMultipleTasks();
+    }
+}
