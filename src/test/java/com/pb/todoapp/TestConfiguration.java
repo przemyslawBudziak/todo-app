ef3089632004ad0ac4e7e7f22eceafbfbe92307d
pb <budziakp@gmail.com>
2023-06-14 11:30:28 +0200

    add TestConfiguration

---
+++ src/test/java/com/pb/todoapp/TestConfiguration.java
@@ -0,0 +1,57 @@
+package com.pb.todoapp;
+
+import com.pb.todoapp.model.Task;
+import com.pb.todoapp.model.TaskRepository;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.*;
+
+@Configuration
+public class TestConfiguration {
+    @Bean
+    @Profile("integration")
+    TaskRepository testRepo() {
+        return new TaskRepository() {
+            private Map<Integer, Task> tasks = new HashMap<>();
+
+            @Override
+            public List<Task> findAll() {
+                return new ArrayList<>(tasks.values());
+            }
+
+            @Override
+            public Page<Task> findAll(Pageable page) {
+                return null;
+            }
+
+            @Override
+            public Optional<Task> findById(Integer id) {
+                return Optional.ofNullable(tasks.get(id));
+            }
+
+            @Override
+            public boolean existsById(Integer id) {
+                return tasks.containsKey(id);
+            }
+
+            @Override
+            public boolean existsByDoneIsFalseAndGroup_Id(Integer groupId) {
+                return false;
+            }
+
+            @Override
+            public List<Task> findByDone(boolean done) {
+                return null;
+            }
+
+            @Override
+            public Task save(Task entity) {
+                return tasks.put(tasks.size() + 1, entity);
+            }
+        };
+    }
+}
